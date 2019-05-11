3463a77b1daf7ff7bb6e2fe60766141aa49cc1d7
Daniyal Nagori <daniyalnagori@yahoo.com>
2019-05-12 00:25:29 +0500

    Fixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
-// import './css/custom.css'
 
 const store = createStore(reducer, middleware)
 
